d779b1d8162f87e4d5c76afaa42868e74f08ef96
doguner1 <d.gunr@yaani.com>
2024-08-13 02:12:04 +0300

    Initial Commit

---
+++ WhatsappClone/WhatsappCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneApp.swift
+//  WhatsappClone
+//
+//  Created by qwerty on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
